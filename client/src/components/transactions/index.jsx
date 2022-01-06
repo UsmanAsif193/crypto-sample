4b76e484c600cd2327e0ac8b24ef8a66ab9491e9
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-01-06 22:58:41 +0500

    TransactionCard in process

--- client/src/components/transactions/index.jsx
+++ client/src/components/transactions/index.jsx
@@ -1,7 +1,51 @@
 import React from "react";
 
+import dummyData from "../../utils/dummyData";
+import { useTransactionContext } from "../../context/transactionContext";
+
+import { shortenAddress } from "../../utils/shortenAddress";
+
+const TransactionCard = ({
+  addressTo,
+  addressFrom,
+  message,
+  keyword,
+  amount,
+  url,
+  timestamp,
+}) => (
+  <div
+    className="bg-[#181918] m-4 flex flex-1 flex-col p-3 rounded-md hover:shadow-2xl 
+    2xl:min-w-[450px]
+    2xl:max-w-[500px]
+    sm:min-w-[270px]
+    sm:max-w-[300px]
+  "
+  ></div>
+);
+
 const Transactions = () => {
-  return <div>This is Transactions</div>;
+  const { currentAccount } = useTransactionContext();
+  return (
+    <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
+      <div className="flex flex-col md:p-12 py-12 px-4">
+        {currentAccount ? (
+          <h3 className="text-white text-3xl text-center my-2">
+            Latest Transactions
+          </h3>
+        ) : (
+          <h3 className="text-white text-3xl text-center my-2">
+            Connect your account to see latest transactions
+          </h3>
+        )}
+        <div className="flex flex-wrap justify-center items-center mt-10">
+          {dummyData.reverse().map((transaction, i) => (
+            <TransactionCard key={i} {...transaction} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Transactions;
--- client/src/components/welcome/index.jsx
+++ client/src/components/welcome/index.jsx
@@ -6,6 +6,7 @@ import { BsInfoCircle } from "react-icons/bs";
 
 import { Loader } from "..";
 import { useTransactionContext } from "../../context/transactionContext";
+import { shortenAddress } from "../../utils/shortenAddress";
 
 const commonStyles =
   "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
@@ -80,7 +81,7 @@ const Welcome = () => {
               </div>
               <div>
                 <p className="text-white font-light text-sm ">
-                  {currentAccount}
+                  {shortenAddress(currentAccount)}
                 </p>
                 <p className="text-white font-semi-bold text-lg ">Ethereum</p>
               </div>
