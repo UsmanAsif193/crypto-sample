5f61689cb1f3131f0cb41a819f2893595859c142
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-01-06 17:57:47 +0500

    Config file errors resolved

--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -3,10 +3,11 @@ require("@nomiclabs/hardhat-waffle");
 module.exports = {
   solidity: "0.8.0",
   networks: {
-    ropsten:
-      "https://eth-ropsten.alchemyapi.io/v2/wpAMeQBmbm6LhMFzpFTDbnY9kZ-5stnt",
-    accounts: [
-      "e1db8ea1598758dce418fbcc87fe4dcb73881bdbb3244a995188cf7607107c6e",
-    ],
+    ropsten: {
+      url: "https://eth-ropsten.alchemyapi.io/v2/wpAMeQBmbm6LhMFzpFTDbnY9kZ-5stnt",
+      accounts: [
+        "e1db8ea1598758dce418fbcc87fe4dcb73881bdbb3244a995188cf7607107c6e",
+      ],
+    },
   },
 };
